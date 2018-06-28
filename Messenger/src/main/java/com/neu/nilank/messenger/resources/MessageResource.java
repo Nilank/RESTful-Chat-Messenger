fb8618dafc4aa4986434d13c0123c88df4ce5d50
Nilank <sharmanilank.com>
2018-06-28 19:16:09 -0400

    Implemented Post Method

--- Messenger/src/main/java/com/neu/nilank/messenger/resources/MessageResource.java
+++ Messenger/src/main/java/com/neu/nilank/messenger/resources/MessageResource.java
@@ -8,7 +8,9 @@ package com.neu.nilank.messenger.resources;
 import com.neu.nilank.messenger.model.Message;
 import com.neu.nilank.messenger.service.MessageService;
 import java.util.List;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -24,14 +26,21 @@ public class MessageResource {
     MessageService messageService = new MessageService();
     
     @GET
-    @Produces(MediaType.APPLICATION_XML)
+    @Produces(MediaType.APPLICATION_JSON)
     public List<Message> getMessages(){
         return messageService.getAllMessages();
     }
     
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Message addMessage(Message message){
+        return messageService.addMessage(message);
+    }
+    
     @GET
     @Path("/{messageId}")
-    @Produces(MediaType.APPLICATION_XML)
+    @Produces(MediaType.APPLICATION_JSON)
     public Message getMessage(@PathParam("messageId") long id){
         return messageService.getMessage(id);
         
