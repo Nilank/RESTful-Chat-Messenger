facd636481c604523376c3657b294a20cc6f4cfc
Nilank <sharmanilank.com>
2018-06-28 16:38:17 -0400

    Created Profile Model and Database

--- Messenger/src/main/java/com/neu/nilank/messenger/service/MessageService.java
+++ Messenger/src/main/java/com/neu/nilank/messenger/service/MessageService.java
@@ -5,9 +5,11 @@
  */
 package com.neu.nilank.messenger.service;
 
+import com.neu.nilank.messenger.database.DatabaseClass;
 import com.neu.nilank.messenger.model.Message;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -15,13 +17,37 @@ import java.util.List;
  */
 public class MessageService {
     
-    public List<Message> getAllMessages(){
-        Message m1 = new Message(1L,"Hello World", "Nilank");
-        Message m2 = new Message(2L, "Hello Jersey", "Nilank");
-        List<Message> list = new ArrayList<>();
-        list.add(m1);
-        list.add(m2);
-        return list;
+    
+    private static Map<Long, Message> messages = DatabaseClass.getMessages();
+    
+    public MessageService(){
+        messages.put(1L, new Message(1, "Hello World", "Nilank"));
+        messages.put(2L, new Message(2, "Hello Jersey", "Nilank"));
     }
     
+    public List<Message> getAllMessages(){
+        return new ArrayList<Message>(messages.values());
+    }
+    
+    public Message getMessage(long id){
+        return messages.get(id);
+    }
+    
+    public Message addMessage(Message message){
+        message.setId(messages.size()+1);
+        messages.put(message.getId(), message);
+        return message;   
+    }
+    
+    public Message updateMessage(Message message){
+        if(message.getId()<=0){
+            return null;
+        }
+        messages.put(message.getId(), message);
+        return message;
+    }
+    
+    public Message removeMessage(long id){
+        return messages.remove(id);
+    }
 }
